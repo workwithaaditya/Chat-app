231b58f56fe69c020d2b97cd9527d2d688e469b3
Harshit Hanabar <harshithanabar09@gmail.com>
2025-10-08 18:02:14 +0530

    Initial commit

---
+++ jsFile/index.js
@@ -0,0 +1,47 @@
+
+const socket = io('http://localhost:8000');
+const form = document.getElementById('send-container');
+const messageInp = document.getElementById('MessageINP');
+const messageContainer = document.querySelector("#container")
+const append = (message, position) => {
+    const messageElement = document.createElement('div');
+    messageElement.classList.add('message');
+    messageElement.classList.add(position);
+    messageElement.innerText = message;
+    messageContainer.append(messageElement);
+    var audio = new Audio('sent.mp3');
+
+}
+
+form.addEventListener('submit',(e)=>{
+    e.preventDefault();
+    const message = messageInp.value;
+    append(`you:${message}`, 'right');
+    socket.emit('send',message);
+    messageInp.value = "";
+    if(position == 'left'){
+
+        audio.play(); 
+    }
+      
+ 
+ 
+
+});
+
+const name = prompt("Enter Your Name to join");
+socket.emit('new-user-joined', name);
+
+socket.on('user-joined', name => {
+    append(`${name} joined the chat`, 'right');
+
+})
+socket.on('receive', data => {
+    append(`${data.name}: ${data.message}`, 'left');
+
+})
+
+socket.on('left', name => {
+    append(`${name}: Has left the chat`, 'left');
+
+})
