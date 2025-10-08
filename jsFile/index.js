915794ec980c0951917819f2036bd8e83ad5bc4e
Harshit Hanabar <harshithanabar09@gmail.com>
2025-10-08 18:37:38 +0530

    Add .gitignore to ignore node_modules

--- jsFile/index.js
+++ jsFile/index.js
@@ -1,5 +1,7 @@
 
-const socket = io('http://localhost:8000');
+// const socket = io('http://localhost:8000');
+const socket = io('https://your-app-name.onrender.com');
+
 const form = document.getElementById('send-container');
 const messageInp = document.getElementById('MessageINP');
 const messageContainer = document.querySelector("#container")
